3923d6a1802c265530bd3e659f8b01d56f131d23
priyanshvijay786 <83647310+priyanshvijay786@users.noreply.github.com>
2021-07-21 12:50:29 +0530

    Create  ASCII Value.c

---
+++ ASCII Value.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+  char var1,var2;
+  var1='h';
+  var2='g';
+  printf("This is designed by Nikhil Bhardwaj ");
+  printf("ASCII values of these are \n");
+  printf("%d %d",var1,var2);
+  if(var1>var2)
+  printf("\nYou are in if");
+  else    
+  printf("\nYou are in else");
+  return 0;
+} 
