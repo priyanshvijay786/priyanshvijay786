8fcbc38029b1942f5d92a16a2d1972d4ab5c0657
priyanshvijay786 <83647310+priyanshvijay786@users.noreply.github.com>
2021-07-21 13:11:53 +0530

    Create Effect of continue.c

---
+++ Effect of continue.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int i,j;
+printf("This is designed by priyansh vijay
+\n");
+for(i=1; i<=2; i++)
+{
+  for(j=1; j<=2; j++)
+  {
+    if(i==j)
+    continue;
+    
+    printf("%d %d\n",i,j);
+  }
+}
+return 0;
+}
