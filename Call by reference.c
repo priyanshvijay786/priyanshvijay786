e3a661e71d33fda84f2b3228037ece25c55ff64d
priyanshvijay786 <83647310+priyanshvijay786@users.noreply.github.com>
2021-07-21 12:57:52 +0530

    Create  Call by reference.c

---
+++ Call by reference.c
@@ -0,0 +1,16 @@
+#include <stdio.h> 
+int funct(int *n)
+{
+ int m=20; 
+ *n= m; 
+ return *n;
+}
+
+ int main()
+{
+ int n=30;
+ printf("This is designed by priyansh vijay\n");
+ printf("%d\n", funct(&n));
+ printf("%d\n", n);
+ return 0;
+}
